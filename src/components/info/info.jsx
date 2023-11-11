2ae9b9edff248bdda24c247e3c58504ae661a3fa
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-11 12:37:04 +0100

    Header complete, Main in progress

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,26 +1,54 @@
-
 const Info = () => {
   return (
-    <header className="header">
-      <div>
-        <p className="header-name">Roberto Quadraccia</p>
-      </div>
-      <nav className="nav-bar">
-        <ul className="nav-bar-list">
-          <li className="nav-bar-item">
-            <a className="nav-bar-link" href="#project">
-              Projects
-            </a>
-          </li>
-          <li className="nav-bar-item">
-            <a className="nav-bar-link" href="#contact">
-              Contact
-            </a>
+    <div>
+      <header className="header">
+        <div>
+          <p className="header-name">Roberto Quadraccia</p>
+        </div>
+        <nav className="nav-bar">
+          <ul className="nav-bar-list">
+            <li className="nav-bar-item">
+              <a className="nav-bar-link" href="#project">
+                Projects
+              </a>
+            </li>
+            <li className="nav-bar-item">
+              <a className="nav-bar-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <button className="btn-resume">Resume</button>
+        </nav>
+      </header>
+      <main className="main">
+        <div className="name-section">
+        <p className="name-section-above">
+          <span>-</span>
+          my name is
+        </p>
+        <h1 className="name-section-title">
+          Roberto 
+          <span className="name-section-title-span">
+            Quadraccia
+          </span>
+        </h1>
+        <p className="name-section-description">
+          Creative full-stack developer with 1 year
+          of experience. Proficient in Python, Javascript (MERN stack)
+          and SQL.
+        </p>
+        <ul className="social-list">
+          <li className="social-item">
+            <a href="https://www.linkedin.com/in/roberto-quadraccia/"></a>
           </li>
         </ul>
-      </nav>
-      <button className="btn-resume">Resume</button>
-    </header>
+        </div>
+        <div className="main-photo-box">
+
+        </div>
+      </main>
+    </div>
   );
 };
 
