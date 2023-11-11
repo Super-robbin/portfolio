6958890743cda95daa47ac653dc95599f1070b29
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-11 17:51:51 +0100

    Main section layout complete

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,3 +1,8 @@
+import linkedIn from "../../images/Linkedin.svg";
+import gitHub from "../../images/Github.svg";
+import instagram  from '../../images/Instagram.svg';
+
+
 const Info = () => {
   return (
     <div>
@@ -23,30 +28,34 @@ const Info = () => {
       </header>
       <main className="main">
         <div className="name-section">
-        <p className="name-section-above">
-          <span>-</span>
-          my name is
-        </p>
-        <h1 className="name-section-title">
-          Roberto 
-          <span className="name-section-title-span">
-            Quadraccia
-          </span>
-        </h1>
-        <p className="name-section-description">
-          Creative full-stack developer with 1 year
-          of experience. Proficient in Python, Javascript (MERN stack)
-          and SQL.
-        </p>
-        <ul className="social-list">
-          <li className="social-item">
-            <a href="https://www.linkedin.com/in/roberto-quadraccia/"></a>
-          </li>
-        </ul>
+          <p className="name-section-above">
+            <span>-</span>
+            my name is
+          </p>
+          <h1 className="name-section-title">
+            Roberto
+            <span className="name-section-title-span">Quadraccia</span>
+          </h1>
+          <p className="name-section-description">
+            Creative full-stack developer with 1 year of experience. Proficient
+            in Python, Javascript (MERN stack) and SQL.
+          </p>
+          <ul className="social-list">
+            <li className="social-item">
+              <a href="https://www.linkedin.com/in/roberto-quadraccia/"><img src={linkedIn} alt="linkedIn-pic"/>
+              </a>
+            </li>
+            <li className="social-item">
+              <a href="https://github.com/Super-robbin"><img src={gitHub} alt="git-pic"/>
+              </a>
+            </li>
+            <li className="social-item">
+              <a href="https://www.instagram.com/super_robbin/"><img src={instagram} alt="insta-pic"/>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="main-photo-box">
-
-        </div>
+        <div className="main-photo-box"></div>
       </main>
     </div>
   );
