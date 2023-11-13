7f9bc69829f7ddb8cd639d4279246af7ceb3cb96
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-13 16:28:54 +0100

    About section setup, working on the tech stack icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Info from "./components/info/Info";
+import About from "./components/about/About";
 import Project from "./components/project/Project";
 // import Contact from "./components/contact/Contact";
 
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <>
       <Info />
+      <About />
       <Project />
       {/* <Contact /> */}
     </>
---
+++ src/components/about/About.jsx
@@ -0,0 +1,49 @@
+const About = () => {
+  return (
+    <div className="about-container">
+      <p className="above-section">
+        <span>- </span>a bit about me
+      </p>
+      <p className="about-section">
+        An energetic, dedicated, and highly focused individual with a solid
+        understanding of software development, databases, and maintenance of
+        complex and critical systems by completing a 16-week software developer
+        bootcamp at MAKERS, London. Aspiring to start the next challenging
+        career as a junior software developer within a tech-oriented environment
+        to develop further skills and experiences. Creative mindset particularly
+        around deployment models and emerging technologies with an aptitude for
+        innovation. Immense knowledge of object-oriented concepts and
+        programming languages, such as JavaScript, Python, HTML, CSS, and SQL.
+      </p>
+      <p className="about-section">
+        My journey into software development began with my enrolment in the
+        highly selective Makers Academy, where I underwent intensive training in
+        cutting-edge development practices. This immersive programme emphasised
+        object-oriented programming (OOP), Agile methodologies, test-driven
+        development (TDD), and the importance of pair programming. This
+        experience not only equipped me with a solid technical foundation but
+        also honed my abilities in collaboration, communication, and
+        problem-solving.
+      </p>
+      <p className="about-section">
+        My career prior to transitioning into software development spanned a
+        decade across various industries, including real estate, food retailing,
+        and hospitality. This diverse background has enriched my skill set with
+        a wealth of soft skills, such as adaptability, effective communication,
+        and problem-solving. I believe these attributes, coupled with my
+        technical expertise, make me a well-rounded candidate ready to excel in
+        a collaborative tech environment.
+      </p>
+      <p className="above-section">
+        <span>- </span>tech stack
+      </p>
+      <div className="stack-section">
+      
+      <i className="devicon-react-original-wordmark colored"></i>
+          
+      </div>
+    </div>
+  );
+};
+
+export default About;
