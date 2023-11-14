c6a9d2311030e8f679f1edf64c3b0430c3979fa0
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-14 14:54:06 +0100

    Contact section layout complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -38,41 +38,39 @@ const About = () => {
         <span>- </span>tech stack
       </p>
       <div className="stack-container">
-        
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
-       
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-      
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
-      
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
-      
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg" />
-          
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg" />
-          
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg" />
       </div>
     </div>
   );
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,27 +1,55 @@
+import message from "../../images/message_light.svg";
+import phone from "../../images/phone_light.svg";
+
 const Contact = () => {
   return (
-    <div className="contact-container">
-      <form className="contact-form">
-        <input
-          className="form-input"
-          placeholder="Name"
-          id="name"
-          type="text"
-        />
-        <input
-          className="form-input"
-          placeholder="Email"
-          id="email"
-          type="text"
-        />
-        <input
-          className="form-input"
-          placeholder="Message"
-          id="message"
-          type="text"
-        />
-        <button className="btn-resume">SEND MESSAGE</button>
-      </form>
+    <div className="contact">
+      <p className="above-section-contact">
+        <span>- </span>contact me
+      </p>
+      <div className="contact-container">
+        <form className="contact-form">
+          <input
+            className="form-input"
+            placeholder="Name"
+            id="name"
+            type="text"
+          />
+          <input
+            className="form-input"
+            placeholder="Email"
+            id="email"
+            type="text"
+          />
+          <textarea
+            className="form-input"
+            placeholder="Message"
+            id="message"
+            type="text"
+          />
+          <button className="btn-message">Send Message</button>
+        </form>
+        <div className="contact-details">
+          <div className="contact-email">
+            <img id="img-email" src={message} alt="message-icon" />
+            <div className="contact-info">
+              <label>E-Mail</label>
+              <p className="email" id="email">
+                quadraccia.rob@gmail.com
+              </p>
+            </div>
+          </div>
+          <div className="contact-number">
+            <img id="img-phone" src={phone} alt="phone-icon" />
+            <div className="contact-info">
+              <label>Phone</label>
+              <p className="phone" id="phone">
+                +44 (0) 7869224777
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
