785bd29d72f5070c12442caa303e1376671d810e
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-19 15:42:46 +0100

    About section vertical update complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,36 +4,46 @@ const About = () => {
       <p className="above-section" id="above-about">
         <span className="dash">-</span>a bit about me
       </p>
-      <p className="about-section">
-        An energetic, dedicated, and highly focused individual with a solid
-        understanding of software development, databases, and maintenance of
-        complex and critical systems by completing a 16-week software developer
-        bootcamp at MAKERS, London. Aspiring to start the next challenging
-        career as a junior software developer within a tech-oriented environment
-        to develop further skills and experiences. Creative mindset particularly
-        around deployment models and emerging technologies with an aptitude for
-        innovation. Immense knowledge of object-oriented concepts and
-        programming languages, such as JavaScript, Python, HTML, CSS, and SQL.
-      </p>
-      <p className="about-section">
-        My journey into software development began with my enrolment in the
-        highly selective Makers Academy, where I underwent intensive training in
-        cutting-edge development practices. This immersive programme emphasised
-        object-oriented programming (OOP), Agile methodologies, test-driven
-        development (TDD), and the importance of pair programming. This
-        experience not only equipped me with a solid technical foundation but
-        also honed my abilities in collaboration, communication, and
-        problem-solving.
-      </p>
-      <p className="about-section">
-        My career prior to transitioning into software development spanned a
-        decade across various industries, including real estate, food retailing,
-        and hospitality. This diverse background has enriched my skill set with
-        a wealth of soft skills, such as adaptability, effective communication,
-        and problem-solving. I believe these attributes, coupled with my
-        technical expertise, make me a well-rounded candidate ready to excel in
-        a collaborative tech environment.
-      </p>
+      <div id="about-p-container">
+        <p className="about-section">
+          An <strong>energetic, dedicated, and highly focused</strong>{" "}
+          individual with a solid understanding of software development,
+          databases, and maintenance of complex and critical systems by
+          completing a 16-week software developer bootcamp at MAKERS, London.
+          Aspiring to start the next challenging career as a{" "}
+          <strong>junior software developer</strong> within a tech-oriented
+          environment to develop further skills and experiences.{" "}
+          <strong>Creative mindset</strong> particularly around deployment
+          models and emerging technologies with an aptitude for <strong>innovation</strong>.
+          Immense knowledge of object-oriented concepts and programming
+          languages, such as
+          <strong> JavaScript, Python, HTML, CSS, and SQL</strong>.
+        </p>
+        <p className="about-section">
+          My journey into software development began with my enrolment in the
+          highly selective Makers Academy, where I underwent intensive training
+          in cutting-edge development practices. This immersive programme
+          emphasised{" "}
+          <strong>
+            object-oriented programming (OOP), Agile methodologies, test-driven
+            development (TDD)</strong>, and the importance of <strong>pair programming
+          </strong>
+          . This experience not only equipped me with a solid technical
+          foundation but also honed my abilities in <strong>collaboration</strong>,
+          communication, and problem-solving.
+        </p>
+        <p className="about-section">
+          My career prior to transitioning into software development spanned a
+          decade across various industries, including real estate, food
+          retailing, and hospitality. This <strong>diverse background</strong> has enriched my
+          skill set with a wealth of soft skills, such as <strong>adaptability,
+          effective communication, and problem-solving</strong>. I believe these
+          attributes, coupled with my technical expertise, make me a
+          well-rounded candidate ready to excel in a collaborative tech
+          environment.
+        </p>
+      </div>
+      <div className="stack-section">
       <p className="above-section" id="above-stack">
         <span className="dash">-</span>tech stack
       </p>
@@ -70,7 +80,8 @@ const About = () => {
 
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
 
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg" />
+        {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg" /> */}
+      </div>
       </div>
     </div>
   );
