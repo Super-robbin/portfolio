0787fb92d680949377506828054f48ed75fcbdf5
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-22 16:29:06 +0100

    Info

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,7 +1,6 @@
 import linkedIn from "../../images/Linkedin.svg";
 import gitHub from "../../images/Github.svg";
 import instagram from "../../images/Instagram.svg";
-import logo from "../../images/logo-r.png";
 import profile from "../../images/profile.jpg"
 
 const Info = () => {
