ed5b46b8f6b49c0865dee2743196c7f29ffab4e8
Roberto Quadraccia <roberto.quadraccia90@gmail.com>
2023-11-22 20:02:43 +0100

    Components renamed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Header from "./components/header/Header";
 import Info from "./components/info/Information";
 import About from "./components/about/About";
 import Project from "./components/project/Projects";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
+import Contact from "./components/contact/Contacts";
+import Footer from "./components/footer/Footers";
 
 const App = () => {
   return (
