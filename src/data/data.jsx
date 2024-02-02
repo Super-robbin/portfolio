468177d1597efcb628a061acd7982a252663c03c
Roberto <roberto.quadraccia90@gmail.com>
2024-02-02 17:55:04 +0100

    Project added

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -2,10 +2,11 @@ import myPlace from "../images/myPlace.jpg";
 import acebook from "../images/acebook.jpg";
 import makersBnb from "../images/MakersBnb-img.jpg";
 import weather from "../images/weather.jpg";
-import projectManagement from "../images/projectManagement.jpg"
+import projectManagement from "../images/projectManagement.jpg";
 import shopManager from "../images/shopManager.jpg";
 import ticTacToe from "../images/ticTacToe.jpg";
 import investmentCalculator from "../images/investmentCalculator.jpg";
+import foodOrderApp from "../images/foodOrderApp.jpg";
 
 const cards = [
   {
@@ -15,7 +16,7 @@ const cards = [
     description:
       "Web App to track visited and desired locations and share your travel map with friends",
     site: "https://myplace-acrry.onrender.com/",
-    github: "https://github.com/Super-robbin/MyPlace"
+    github: "https://github.com/Super-robbin/MyPlace",
   },
   {
     id: 2,
@@ -34,14 +35,24 @@ const cards = [
   },
   {
     id: 4,
+    img: foodOrderApp,
+    name: "Food Order App",
+    description:
+      "A food order app where users can browse food items, update the shopping cart and finally be taken to a form to finalise the order.",
+    site: "https://food-order-app-81he.onrender.com/",
+    github: "https://github.com/Super-robbin/food_order_app",
+  },
+  {
+    id: 5,
     img: projectManagement,
     name: "Project Management App",
-    description: "A web app to manage projects and tasks, created using React and styled with Tailwind CSS",
+    description:
+      "A web app to manage projects and tasks, created using React and styled with Tailwind CSS",
     site: "https://project-management-app-2jrp.onrender.com/",
     github: "https://github.com/Super-robbin/project_management_app",
   },
   {
-    id: 5,
+    id: 6,
     img: weather,
     name: "Weather App",
     description: "A web app displaying current weather and weekly forecasts",
@@ -49,7 +60,7 @@ const cards = [
     github: "https://github.com/Super-robbin/weather_app",
   },
   {
-    id: 6,
+    id: 7,
     img: investmentCalculator,
     name: "Investment Calculator",
     description: "A web app to calculate return on investment",
@@ -57,7 +68,7 @@ const cards = [
     github: "https://github.com/Super-robbin/investment_calculator",
   },
   {
-    id: 7,
+    id: 8,
     img: ticTacToe,
     name: "Tic-Tac-Toe",
     description: "A web app to play the iconic game",
@@ -65,7 +76,7 @@ const cards = [
     github: "https://github.com/Super-robbin/tic_tac_toe",
   },
   {
-    id: 8,
+    id: 9,
     img: shopManager,
     name: "Shop Manager",
     description: "A back-end shop manager project created with Python",
